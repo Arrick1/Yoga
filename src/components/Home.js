5b3d781b108ea595fc68aee0e5dbd5b397d2ea7a
Arrick <Grapes01@gmail.com>
2019-08-09 10:26:35 -0400

    setup

---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+
+
+/* <------- Imported Components -------> */
+import AbsoluteWrapper from './AbsoluteWrapper'
+
+
+
+const Home = () => {
+    return(
+        <AbsoluteWrapper>
+            <div>
+                This is the home 
+            </div>
+
+        </AbsoluteWrapper>
+
+    )
+   
+}
+
+
+
+export default Home;
